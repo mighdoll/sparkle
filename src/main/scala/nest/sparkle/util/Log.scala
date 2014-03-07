3f269697f6cc3ea8ef9738ea8bcc4d36d9cc3754
Lee Mighdoll <lee@nestlabs.com>
2014-03-06 17:56:51 -0800

    fix scaladoc reference

--- src/main/scala/nest/sparkle/util/Log.scala
+++ src/main/scala/nest/sparkle/util/Log.scala
@@ -2,11 +2,8 @@ package nest.sparkle.util
 import org.slf4j.LoggerFactory
 import com.typesafe.scalalogging.slf4j.Logger
 
-/** Adds the lazy val 'log' of type [[$Logger]] to the class into which this trait is mixed.
-  *
-  * @define Logger com.typesafe.scalalogging.slf4j.Logger
+/** Adds a lazy val 'log' of type com.typesafe.scalalogging.slf4j.Logger to the class into which this trait is mixed.
   */
-
 trait Log {
   protected lazy val log: Logger = Logger(LoggerFactory.getLogger(getClass.getName))
 }
