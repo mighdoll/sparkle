016282517136aaef2bf89e5c5edf06056ce66209
Lee Mighdoll <lee@nestlabs.com>
2015-02-16 11:55:46 -0800

    remove comment junk

--- store-test-kit/src/main/scala/nest/sparkle/store/cassandra/ActorSystemFixture.scala
+++ store-test-kit/src/main/scala/nest/sparkle/store/cassandra/ActorSystemFixture.scala
@@ -4,9 +4,6 @@ import akka.actor.ActorSystem
 
 import nest.sparkle.util.RandomUtil._
 
-/**
- * Created by mighdoll on 1/30/15.
- */
 object ActorSystemFixture {
   /** run a function within a test actor system */
   def withTestActors[T](name:String = "test-actors")(fn: ActorSystem => T): T = {
