78ef5a7c0e6a3c9259ff2dabc32d8d59393aab99
Lee Mighdoll <lee@nestlabs.com>
2015-02-23 17:53:37 -0800

    simplify reduction transform grouping by calculating grouping on demand instead of attaching the grouping to the stream . get rid of AsyncWithRangeReduction

--- protocol/src/main/scala/nest/sparkle/time/transform/ReductionTransform.scala
+++ protocol/src/main/scala/nest/sparkle/time/transform/ReductionTransform.scala
@@ -10,6 +10,7 @@ import com.typesafe.config.Config
 import spray.json.JsObject
 
 import nest.sparkle.measure.{Measurements, Span, TraceId}
+import nest.sparkle.store.Column
 import nest.sparkle.time.protocol.{SummaryParameters, JsonDataStream, JsonEventWriter, KeyValueType}
 import nest.sparkle.time.transform.FetchStreams.fetchData
 import nest.sparkle.util.{Log, PeriodWithZone, RecoverNumeric}
@@ -55,9 +56,10 @@ case class ReduceTransform[V]
     ( rootConfig: Config,
       produceReduction: TypeTag[_] => Try[IncrementalReduction[V]] )
     ( implicit measurements: Measurements )
-  extends MultiTransform with Log {
+    extends MultiTransform with Log {
 
   private val validator = ValidateReductionParameters()
+  private val noGrouping = StreamGrouping(maxParts = maxParts, None)
 
   override def transform // format: OFF
       ( futureGroups: Future[Seq[ColumnGroup]], transformParameters: JsObject)
@@ -72,8 +74,7 @@ case class ReduceTransform[V]
           keyType, keyJsonFormat, keyOrdering, reductionParameters, grouping, ongoingDuration
         ) <- validator.validate[K](futureGroups, transformParameters)
         data <- fetchData[K,V](futureGroups, reductionParameters.ranges, None, Some(span))
-        dataWithReduction = attachReduction(data, grouping )
-        reduced = reduceOperation[K](produceReduction, dataWithReduction, ongoingDuration)
+        reduced = reduceOperation[K](data, produceReduction, grouping, ongoingDuration)
       } yield {
         reduced.allStreams.map { stream =>
           val json = JsonEventWriter.fromDataStream(stream, span)
@@ -88,88 +89,11 @@ case class ReduceTransform[V]
     withFixedKeyType[Any]
   }
 
-  /** Attach the partitioning instructions to each individual stream.
-    *
-    * Clients can specify any of the groupings that subclass RequestGrouping which includes
-    * more groupings than the stream reduction code can handle. The stream reduction
-    * only supports two groupings: by date and by count. This routine converts the more complex
-    * groupings that require extra fetches from the database (such as IntoCountedParts) into
-    * simpler ones e.g. ByCount.  */
-  def attachReduction[K]
-      ( streams: StreamGroupSet[K, V, AsyncWithRangeColumn], grouping: Option[RequestGrouping] )
-      ( implicit execution: ExecutionContext, parentSpan:Span )
-      : StreamGroupSet[K, V, AsyncWithRangeReduction] = {
-    grouping match {
-      case Some(IntoCountedParts(count)) =>
-        countedPartsToByCount(streams, count)
-      case Some(grouping@ByCount(count)) =>
-        attachReductionToAllStreams(streams, StreamGrouping(maxParts, Some(grouping)))
-      case Some(grouping@ByDuration(duration)) =>
-        attachReductionToAllStreams(streams, StreamGrouping(maxParts, Some(grouping)))
-      case Some(IntoDurationParts(count)) =>
-        durationPartsToByDuration(streams, count)
-      case None => attachReductionToAllStreams(streams, noGrouping)
-    }
-  }
-
-  val noGrouping = StreamGrouping(maxParts = maxParts, None)
-
-  /** Convert IntoDurationParts into a ByDuration reduction and attach it to the streams.
-    * The approach is to request the total duration from the database and then issue a ByDuration
-    * reduction request with the appropriately rounded time duration. */
-  private def durationPartsToByDuration[K]
-      ( streams: StreamGroupSet[K, V, AsyncWithRangeColumn], intoParts:Int )
-      ( implicit execution: ExecutionContext, parentSpan:Span )
-      : StreamGroupSet[K, V, AsyncWithRangeReduction] = {
-    ???
-  }
-
-  /** attach a StreamGrouping to all the streams in a StreamGroupSet */
-  private def attachReductionToAllStreams[K]
-      ( streams: StreamGroupSet[K, V, AsyncWithRangeColumn], grouping: StreamGrouping )
-      ( implicit execution: ExecutionContext, parentSpan:Span )
-      : StreamGroupSet[K, V, AsyncWithRangeReduction] = {
-
-    val futureGrouping = Future.successful(grouping)
-    streams.mapStreams { stream =>
-      import stream.keyType
-      import stream.valueType
-      val newStream = new AsyncWithRangeReduction(stream.self.initial, stream.self.ongoing,
-        stream.self.requestRange, futureGrouping)
-      newStream.asInstanceOf[TwoPartStream[K,V, AsyncWithRangeReduction]] // SCALA ?
-    }
-  }
-
-
-  /** Convert IntoCountedParts into a ByCount reduction and attach it to the streams.
-    * The approach is to request the total count from the database and then issue a ByCount
-    * reduction request with target_per_part_count = total_count / target_number_of_parts */
-  private def countedPartsToByCount[K]
-      ( streams: StreamGroupSet[K, V, AsyncWithRangeColumn], intoParts:Int )
-      ( implicit execution: ExecutionContext, parentSpan:Span )
-      : StreamGroupSet[K, V, AsyncWithRangeReduction] = {
-
-    streams.mapStreams { stream =>
-      import stream.keyType
-      import stream.valueType
-      val optStart = stream.self.requestRange.flatMap(_.start)
-      val optUntil = stream.self.requestRange.flatMap(_.until)
-      val futureGrouping =
-        stream.self.column.countItems(optStart, optUntil).map { totalCount =>
-          val countPerPart = math.ceil(totalCount.toDouble / intoParts).toInt
-          StreamGrouping(maxParts, Some(ByCount(countPerPart)))
-        }
-      val newStream = new AsyncWithRangeReduction(stream.self.initial, stream.self.ongoing,
-        stream.self.requestRange, futureGrouping
-      )
-      newStream.asInstanceOf[TwoPartStream[K,V, AsyncWithRangeReduction]] // SCALA ?
-    }
-  }
-
   /** Perform a reduce operation on all the streams in a group set.  */
   private def reduceOperation[K] // format: OFF
-      ( makeReduction: TypeTag[_] => Try[IncrementalReduction[V]],
-        groupSet: StreamGroupSet[K, V, AsyncWithRangeReduction],
+      ( groupSet: StreamGroupSet[K, V, AsyncWithRangeColumn],
+        makeReduction: TypeTag[_] => Try[IncrementalReduction[V]],
+        requestGrouping: Option[RequestGrouping],
         ongoingDuration: Option[FiniteDuration] )
       ( implicit execution: ExecutionContext, parentSpan: Span )
       : StreamGroupSet[K, Option[V], AsyncWithRange] = { // format: ON
@@ -179,9 +103,11 @@ case class ReduceTransform[V]
       implicit val valueType = stream.valueType
 
       val reduced: Future[AsyncWithRange[K,Option[V]]] = {
+        val futureGrouping =
+          streamGrouping(requestGrouping, stream.self.requestRange, stream.self.column)
         for {
           reduction <- makeReduction(valueType).toFuture
-          reduced <- stream.self.flexibleReduce(stream.self.reductionGrouping, reduction, ongoingDuration)
+          reduced <- stream.self.flexibleReduce(futureGrouping, reduction, ongoingDuration)
         } yield {
           reduced
         }
@@ -189,5 +115,71 @@ case class ReduceTransform[V]
       AsyncWithRange.flattenFutureAsyncWithRange(reduced, stream.self.requestRange)
     }
   }
+
+
+  /** Convert any difficult (Into*) partitioning into simple (By*) partitionings.
+    * e.g. convert IntoCountedParts into ByCount by fetching the total number of
+    * items in the column and dividing by the target number of parts
+    *
+    * Clients can specify any of the groupings that subclass RequestGrouping which includes
+    * more groupings than the stream reduction code can handle. The stream reduction
+    * only supports two groupings: by date and by count. This routine converts the more complex
+    * groupings that require extra fetches from the database (such as IntoCountedParts) into
+    * simpler ones e.g. ByCount.  */
+  private def streamGrouping[K]
+      ( requestGrouping:Option[RequestGrouping],
+        requestRange: Option[SoftInterval[K]],
+        column:Column[K,V] )
+      ( implicit execution: ExecutionContext, parentSpan: Span )
+      : Future[StreamGrouping] = {
+    requestGrouping match {
+      case Some(grouping@ByCount(count)) =>
+        Future.successful(StreamGrouping(maxParts, Some(grouping)))
+      case Some(grouping@ByDuration(duration)) =>
+        Future.successful(StreamGrouping(maxParts, Some(grouping)))
+      case None =>
+        Future.successful(noGrouping)
+      case Some(IntoCountedParts(count)) =>
+        countedPartsToByCount(count, requestRange, column).map { byCount =>
+          StreamGrouping(maxParts, Some(byCount))
+        }
+      case Some(IntoDurationParts(count)) =>
+        durationPartsToByDuration(count, requestRange, column).map { byDuration =>
+          StreamGrouping(maxParts, Some(byDuration))
+        }
+    }
+  }
+
+  /** Convert IntoCountedParts into a ByCount reduction and attach it to the streams.
+    * The approach is to request the total count from the database and then issue a ByCount
+    * reduction request with target_per_part_count = total_count / target_number_of_parts */
+  private def countedPartsToByCount[K]
+      ( intoParts:Long, requestRange:Option[SoftInterval[K]], column:Column[K,V] )
+      ( implicit execution: ExecutionContext, parentSpan: Span )
+      : Future[ByCount] = {
+
+    val optStart = requestRange.flatMap(_.start)
+    val optUntil = requestRange.flatMap(_.until)
+    val futureGrouping =
+      column.countItems(optStart, optUntil).map { totalCount =>
+        val countPerPart = math.ceil(totalCount.toDouble / intoParts).toInt
+        ByCount(countPerPart)
+      }
+    futureGrouping
+  }
+
+  /** Convert IntoDurationParts into a ByDuration reduction and attach it to the streams.
+    * The approach is to request the total duration from the database and then issue a ByDuration
+    * reduction request with the appropriately rounded time duration. */
+  private def durationPartsToByDuration[K]
+      ( intoParts:Long, requestRange:Option[SoftInterval[K]], column:Column[K,V] )
+      ( implicit execution: ExecutionContext, parentSpan: Span )
+      : Future[ByDuration] = {
+
+    val optStart = requestRange.flatMap(_.start)
+    val optUntil = requestRange.flatMap(_.until)
+    ???
+  }
+
 
 }
