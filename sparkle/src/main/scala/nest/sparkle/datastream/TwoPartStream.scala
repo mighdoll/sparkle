092a0d776aa8e865bd2a39de36688bed65481c77
Lee Mighdoll <lee@nestlabs.com>
2015-09-04 15:26:59 -0700

    revise comments about TwoPartStream, StreamGroupSet, etc.

--- sparkle/src/main/scala/nest/sparkle/datastream/TwoPartStream.scala
+++ sparkle/src/main/scala/nest/sparkle/datastream/TwoPartStream.scala
@@ -7,8 +7,7 @@ import rx.lang.scala.Observable
 import scala.{ specialized => spec }
 
 // format: OFF
-// TODO revise this documentation to match the current code
-/** The data transformations specified by protocol requests work on data from Columns in the Store. 
+/** The data transformations specified by protocol requests work on data from Columns in the Store.
   * Transforms may operate on multiple slices of data from one column, on multiple columns, and on 
   * multiple groups of columns. The containers of the data are organized hierarchically, as follows:
   *
@@ -16,23 +15,24 @@ import scala.{ specialized => spec }
   *   StreamGroup - data from multiple columns
   *     StreamStack - data from one or more slices of a single column 
   *       TwoPartStream - data from a single slice from a single column
-  *                    implementations of TwoPartStream have two collections of data
+  *                    note that internally TwoPartStream has two collections of data
   *         initial - all data available the time of the request
   *         ongoing - items arriving after the request (normally only for open ended ranges)
   *         
   *         DataArray - both initial and ongoing data contain sample data packed into DataArrays
-  *                     The DataArrays are delivered asynchronously: buffered in a Future or streamed in an Observable
+  *                     The DataArrays are delivered asynchronously: buffered in a Future or
+  *                     streamed in an Observable.
   * 
   * Conventions for type parameter letters:
   *   K - key type
   *   V - value type
-  *   S - TwoPartStream typeclass proxy type
+  *   S - TwoPartStream type constructor
   *  
   *   B - target value type (e.g. for mapData)
-  *   T - target TwoPartStream typeclass proxy type (e.g. for mapStream)
+  *   T - target TwoPartStream type constructor (e.g. for mapStream)
   * 
   * There are two core challenges in structuring the container data structures: 1) mapping higher level
-  * functions through the layers of the containment heirarchy: e.g. we want to enable users of the 
+  * functions through the layers of the containment hierarchy: e.g. we want to enable users of the
   * library to convert on/off boolean values to duration lengths without having to worry about the 
   * four levels of containment and various subtypes involved. 2) enable users of the library to
   * operate memory-efficiently on arrays in a generic way: e.g. library users should be able to 
@@ -46,28 +46,28 @@ import scala.{ specialized => spec }
   *     should fail at compile time if applied to doubles, or non-buffered streams.
   *   . Nested building: The library needs a way to construct new TwoPartStream subtype instances, e.g. when mapping
   *     to from Long to Boolean elements types. 
-  *   . Minimal boilerplate, especially for clients of the library. Naiive solutions that e.g. push 
-  *     the nested building problem onto users of the library are unnattractive.
+  *   . Minimal boilerplate, especially for clients of the library. Pushing the nested building
+  *     problem onto users of the library is not attractive.
   * 
   * This is similar to the challenge tackled by scala collection libraries: the scala collection library, 
   * scalaz, debox, psp-std, etc.
   * 
-  * I'm aware of three broad approaches to this problem: higher kinded Builders, F-bounded types, and 
-  * higher kinded proxying Typeclasses. At the moment, we're using the proxying typeclass approach favored 
-  * by e.g. scalaz and spire. (or at least my imitation of that technique)  
-  * // SCALA is this the best approach? f-bounded seems difficult, but builders might be sufficient. 
-  * // builders wouldn't as easily expose custom functionality on contained types though..
+  * I'm aware of a few broad approaches to this problem: higher kinded Builders, F-bounded types, and
+  * higher kinded proxying Typeclasses. Here, we're using higher kinded types to specify
+  * the implementation of the TwoPartStream. We build new new TwoPartStream kinds explicitly via
+  * mapStreams. Implementations of TwoPartStreams build their own instances inside their own
+  * combinator implementations (mapData, doOnEach, and plus). This could be extended to the
+  * Builder approach, though I'd suggest trying the proxying typeclass approach if this area
+  * of the system needs to grow in complication.
   * 
   * -- 2) Efficient Array functions --
   * JVM primitive types (long, double, etc.) are efficient for both computation and storage. Naiive 
   * implementations that work on generic types will tend to create boxed versions of the primitive values
-  * and make overloaded function calls to do basic arithmatic and comparison (e.g. add). 
-  * 
-  * Our current approach is to use spire and specialization for efficient functions on primitive arrays. The 
-  * basic approach is to a) use specialization to ask the compiler to create duplicate primitive-optimized 
-  * versions of performance critical inner loops (and the calling chain required to trigger those inner loops) and b) rely
-  * on spire's carefully tuned functions and typeclasses (themselves specialized) for generic operations.
-  */ 
+  * and make overloaded function calls to do basic arithmetic and comparison (e.g. add).
+  *
+  * Currently the computation paths are not optimized. There are some experiments with
+  * specialization documented in SpecialDataArray.
+  */
 
 // format: ON
 
